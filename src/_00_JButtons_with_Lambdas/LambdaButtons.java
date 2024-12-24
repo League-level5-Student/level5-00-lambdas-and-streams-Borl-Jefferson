01cc8f9d4567d7985fff0f3b6f9fe4a9ceff7d20
Aarit Jayaswal <aaritrj@gmail.com>
2024-12-23 18:19:52 -0800

    StreamSorter line 28 make it output correct

--- src/_00_JButtons_with_Lambdas/LambdaButtons.java
+++ src/_00_JButtons_with_Lambdas/LambdaButtons.java
@@ -1,11 +1,15 @@
 package _00_JButtons_with_Lambdas;
 
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
-public class LambdaButtons {
+public class LambdaButtons implements ActionListener {
 	private JFrame window = new JFrame();
 	private JButton addNumbers = new JButton("ADD 2 Numbers");
 	private JButton randNumber = new JButton("RANDOM NUMBER");
@@ -20,6 +24,48 @@ public class LambdaButtons {
 		
 		//1. Call the addActionListener methods for each button. Use lambdas
 		//   to define to functionality of the buttons.
+		addNumbers.addActionListener( e -> {
+			int numadd=0;
+			Boolean broke=false;
+			String add = JOptionPane.showInputDialog("Number 1 / 2");
+			try {
+			numadd = Integer.parseInt(add);
+			System.out.print(numadd);
+			}catch (NumberFormatException t) {
+				JOptionPane.showMessageDialog(null, "String inputed was not an int.");
+				broke=true;
+			}
+			if(!broke) {
+				String add2 = JOptionPane.showInputDialog("Number 2 / 2");
+				try {
+				int numadd2 = Integer.parseInt(add2);
+				System.out.println(" + " + numadd2 + " = " + (numadd+numadd2));
+				}catch (NumberFormatException t) {
+					JOptionPane.showMessageDialog(null, "String inputed was not an int");
+				}
+			}
+			
+		});
+		
+		randNumber.addActionListener( e -> {
+		String mm = JOptionPane.showInputDialog("Enter the lowest and highest number seperated by a comma and space");
+		String[] split = mm.split(", ");
+		int min = Integer.parseInt(split[0]);
+		int max = Integer.parseInt(split[1]);
+		if(min>max) {
+			int p = min;
+			min=max;
+			max=p;
+		}
+		System.out.println("min: "+min + " | max: "+max);
+		Random r = new Random();
+		System.out.println(r.nextInt(max+1-min)+min);
+		});
+		
+		tellAJoke.addActionListener( e -> {
+			String joke = JOptionPane.showInputDialog("Tell me a joke");
+			System.out.println("Wanna hear a joke?\n"+joke);
+		});
 		
 		window.setVisible(true);
 		window.pack();
@@ -27,6 +73,12 @@ public class LambdaButtons {
 	
 	public static void main(String[] args) {
 		new LambdaButtons();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 	
 	
--- src/_01_Lambda_Methods/LambdaMethods.java
+++ src/_01_Lambda_Methods/LambdaMethods.java
@@ -1,26 +1,69 @@
 package _01_Lambda_Methods;
 
+import java.util.Iterator;
+
 public class LambdaMethods {
 	public static void main(String[] args) {
 		// 1. Look at the SpecialPrinter function interface.
-	
+	String in = "repeat";
 		// Here is an example of calling the printCustomMessage method with a lambda.
 		// This prints the passed in String 10 times.
 		printCustomMessage((s)->{
 			for(int i = 0; i < 10; i++) {
 				System.out.println(s);
 			}
-		}, "repeat");
+		}, in);
 		
 		//2. Call the printCustonMessage method using a lambda so that the String prints backwards.
+		System.out.println();
 		
+		printCustomMessage( (s) -> {
+			String out = "";
+			for(int i = in.length(); i > 0; i--) {
+				out+=in.substring(i-1, i);
+			}
+			System.out.println(out);
+		}, in);
 		//3. Call the printCustonMessage method using a lambda so that the String prints with a mix between upper an lower case characters.
+		System.out.println();
 		
+		printCustomMessage( (s) -> {
+			String out = "";
+			for(int i = 0; i < in.length(); i++) {
+				if(i%2==1) {
+					out+=in.substring(i, i+1).toUpperCase();
+				}
+				else {
+					out+=in.substring(i, i+1).toLowerCase();
+				}
+			}
+			System.out.println(out);
+		}, in);
 		//4. Call the printCustonMessage method using a lambda so that the String prints with a period in between each character.
+		System.out.println();
 		
+		printCustomMessage( (s) -> {
+			String out = in.substring(0, 1);
+			for(int i = 1; i < in.length(); i++) {
+				out+="."+in.substring(i, i+1);
+			}
+			System.out.println(out);
+		}, in);
 		//5. Call the printCustonMessage method using a lambda so that the String prints without any vowels.
-	
+		System.out.println();
+		
+		printCustomMessage( (s) -> {
+			String out = "";
+			for(int i = 0; i < in.length(); i++) {
+				String j = in.substring(i, i+1);
+				if(!j.equals("a") && !j.equals("e") && !j.equals("u") && !j.equals("i") && !j.equals("o")) {
+					out+=j;
+				}
+			}
+			System.out.println(out);
+		}, in);
 	}
+	
 	
 	public static void printCustomMessage(SpecialPrinter sp, String value) {
 		sp.printSpecial(value);
